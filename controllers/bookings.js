cf3ed8af4866f92a2256a3e23ffe96ff6501bd97
nirajan <nirajankarki2015@gmail.com>
2023-10-11 11:32:02 +0200

    Refactor done

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -8,7 +8,7 @@ const Booking = db.Booking;
  * @param {Object} res - The response object used to send the response.
  * @return {Object} The created booking object.
  */
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
   const { startTime, endTime, userId, roomId, bookingTitle } = req.body;
 
   // Validate request
@@ -19,58 +19,63 @@ exports.create = (req, res) => {
     return;
   }
 
-  // Check if User and Room exist
-  Promise.all([db.User.findByPk(userId), db.Room.findByPk(roomId)])
-    .then(([user, room]) => {
-      if (!user || !room) {
-        res.status(400).send({
-          message: "User or Room not found!",
-        });
-        return;
-      }
+  try {
+    // Check if User and Room exist
+    const [user, room] = await Promise.all([
+      db.User.findByPk(userId),
+      db.Room.findByPk(roomId),
+    ]);
 
-      // Check if there's already a booking in the specified time range for the same room
-      return Booking.findOne({
-        where: {
-          roomId: roomId,
-          startTime: {
-            [db.Sequelize.Op.lt]: new Date(endTime),
-          },
-          endTime: {
-            [db.Sequelize.Op.gt]: new Date(startTime),
-          },
-        },
+    // Also can use below alternative code
+
+    /* const user= await db.User.findByPk(userId);
+    const room= await db.Room.findByPk(roomId);
+ */
+    if (!user || !room) {
+      res.status(400).send({
+        message: "User or Room not found!",
       });
-    })
-    .then((existingBooking) => {
-      if (existingBooking) {
-        res.status(400).send({
-          message: "The room is already booked for the specified time range.",
-        });
-        return;
-      }
+      return;
+    }
 
-      // Create a booking
-      const booking = {
-        startTime: startTime,
-        endTime: endTime,
-        userId: userId,
+    // Check if there's already a booking in the specified time range for the same room
+    const existingBooking = await Booking.findOne({
+      where: {
         roomId: roomId,
-        bookingTitle: bookingTitle,
-      };
-
-      // Save Booking in the database
-      return Booking.create(booking);
-    })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Booking.",
-      });
+        startTime: {
+          [db.Sequelize.Op.lt]: new Date(endTime),
+        },
+        endTime: {
+          [db.Sequelize.Op.gt]: new Date(startTime),
+        },
+      },
     });
+
+    if (existingBooking) {
+      res.status(400).send({
+        message: "The room is already booked for the specified time range.",
+      });
+      return;
+    }
+
+    // Create a booking
+    const booking = {
+      startTime: startTime,
+      endTime: endTime,
+      userId: userId,
+      roomId: roomId,
+      bookingTitle: bookingTitle,
+    };
+
+    // Save Booking in the database
+    const data = await Booking.create(booking);
+
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the Booking.",
+    });
+  }
 };
 
 /**
@@ -80,21 +85,21 @@ exports.create = (req, res) => {
  * @param {Object} res - The response object.
  * @return {Object} The response object.
  */
-exports.getAll = (req, res) => {
-  Booking.findAll({})
-    .then((bookings) => {
-      res.status(200).json({
-        success: true,
-        data: bookings,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Server error",
-        error: err.message,
-      });
+
+exports.getAll = async (req, res) => {
+  try {
+    const bookings = await Booking.findAll({});
+    res.status(200).json({
+      success: true,
+      data: bookings,
     });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
 };
 
 /**
@@ -104,30 +109,30 @@ exports.getAll = (req, res) => {
  * @param {Object} res - The response object.
  * @return {Object} The JSON response containing the booking data or an error message.
  */
-exports.getById = (req, res) => {
+exports.getById = async (req, res) => {
   const id = req.params.id;
 
-  Booking.findByPk(id)
-    .then((booking) => {
-      if (booking) {
-        res.status(200).json({
-          success: true,
-          data: booking,
-        });
-      } else {
-        res.status(404).json({
-          success: false,
-          message: "Booking not found",
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Server error",
-        error: err.message,
+  const booking = await Booking.findByPk(id);
+
+  try {
+    if (booking) {
+      res.status(200).json({
+        success: true,
+        data: booking,
       });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Booking not found",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
     });
+  }
 };
 
 /**
@@ -137,35 +142,36 @@ exports.getById = (req, res) => {
  * @param {Object} res - The response object.
  * @return {Promise} A promise that resolves to the updated booking.
  */
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
   const updatedData = req.body;
 
-  Booking.findByPk(id)
-    .then((booking) => {
-      if (booking) {
-        return booking.update(updatedData); // Update the booking
-      } else {
-        res.status(404).json({
-          success: false,
-          message: "Booking not found",
-        });
-      }
-    })
-    .then((updatedBooking) => {
-      res.status(200).json({
-        success: true,
-        data: updatedBooking,
-        message: "Booking updated successfully",
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
+  const booking = await Booking.findByPk(id);
+
+  try {
+    const booking = await Booking.findByPk(id);
+
+    if (!booking) {
+      res.status(404).json({
         success: false,
-        message: "Server error",
-        error: err.message,
+        message: "Booking not found",
       });
+      return;
+    }
+
+    const updatedBooking = await booking.update(updatedData);
+    res.status(200).json({
+      success: true,
+      data: updatedBooking,
+      message: "Booking updated successfully",
     });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
 };
 /**
  * Deletes a booking.
@@ -174,31 +180,30 @@ exports.update = (req, res) => {
  * @param {Object} res - The response object.
  * @returns {Promise} A promise that resolves to the deleted booking or an error.
  */
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
 
-  Booking.findByPk(id)
-    .then((booking) => {
-      if (booking) {
-        return booking.destroy(); // Delete the booking
-      } else {
-        res.status(404).json({
-          success: false,
-          message: "Booking not found",
-        });
-      }
-    })
-    .then(() => {
-      res.status(200).json({
-        success: true,
-        message: "Booking deleted successfully",
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
+  try {
+    const booking = await Booking.findByPk(id);
+
+    if (!booking) {
+      res.status(404).json({
         success: false,
-        message: "Server error",
-        error: err.message,
+        message: "Booking not found",
       });
+      return;
+    }
+
+    await booking.destroy();
+    res.status(200).json({
+      success: true,
+      message: "Booking deleted successfully",
     });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
 };
